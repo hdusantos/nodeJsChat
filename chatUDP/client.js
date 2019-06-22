063bda55c44c4d8ae120a1bd3bdfbf2f9f843885
Hudson dos Santos <hudson.alves@live.com>
2019-06-22 12:57:45 -0300

    Using cluster

--- chatUDP/client.js
+++ chatUDP/client.js
@@ -15,29 +15,33 @@ client.on("error", (err) => {
 
 client.on("message", (msg, rinfo) => {
     console.log(`${msg}`);
+    process.stdout.write("> ");
 });
 
 client.on("listening", () => {
     const address = client.address();
     console.log(`Client: ${address.address}:${address.port}`);
-    console.log("Digite 'exit' para sair\n");
+    console.log("Digite '/exit' para sair\n");
 });
 
 const room = Buffer.from(process.argv[2]);
 const roomSend = Buffer.from(`${room}|Entrou na Sala`);
-client.send(roomSend, portServer, host);
-
+client.send(roomSend, portServer, host, () => {
+    process.stdout.write("> ");
+});
 
 process.stdin.on("data", (data) => {
     let message = data.toString().replace(/\n|\n/g, "");
-    if (message == "exit") {
+    if (message == "/exit") {
         const message = Buffer.from(`${room}|--- Saiu da Sala ---`);
         client.send(message, portServer, host, (err) => {
             client.close();
             process.exit();
         });
-    } else {
+    } else if (message.length > 0){
         message = Buffer.from(`${room}|${message}`);
         client.send(message, portServer, host);
+        process.stdout.write("> ");
     }
 });
+
--- chatUDP/server.js
+++ chatUDP/server.js
@@ -1,84 +1,102 @@
+// Escalar aplicação
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
+// Utilizado para I/O do arquivo json
 const fs = require("fs");
 const { spawnSync } = require( 'child_process' );
+
+// Uso do socket
 const dgram = require("dgram");
 const server = dgram.createSocket({
     type: "udp4",
     reuseAddr: true
 });
 
-const host = "localhost";
-const portServer = 6001;
+// Escala a aplicação para o numero de CPUs
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const host = "localhost";
+    const portServer = 6001;
 
-let rooms = {}
+    // Estrutura para armazenar sala e usuarios contidos em cada sala
+    let rooms = {}
 
-// Checa se eh a primeira instancia do servidor na porta 6001
-const checkInstanceServer = () => {
-    lsof = spawnSync( 'lsof', [ '-i', 'udp:6001' ] );
-    console.log(`${lsof.stdout}`);
-    if (!lsof.stdout.toString().length) {
-        fs.writeFileSync("rooms.json", "{}",{enconding:'utf-8',flag: 'w'}, (err) => {
+    // Checa se eh a primeira instancia do servidor na porta 6001
+    const checkInstanceServer = () => {
+        lsof = spawnSync( 'lsof', [ '-i', 'udp:6001' ] );
+        if (!lsof.stdout.toString().length) {
+            fs.writeFileSync("rooms.json", "{}",{enconding:'utf-8',flag: 'w'}, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        } else {
+            rooms = JSON.parse(fs.readFileSync("rooms.json", "utf-8"));
+        }
+    }
+    checkInstanceServer();
+
+    // Grava os dados da sala em um arquivo JSON
+    const writeRooms = () => {
+        fs.writeFile('rooms.json', JSON.stringify(rooms), 'utf8', (err) => {
             if (err) {
-                console.log(err);
+                console.log(err)
             }
         });
-    } else {
+    }
+
+    // Le sala do arquivo Json
+    const readRooms = () => {
         rooms = JSON.parse(fs.readFileSync("rooms.json", "utf-8"));
     }
-}
-checkInstanceServer();
 
-// Grava os dados da sala em um arquivo JSON
-const writeRooms = () => {
-    fs.writeFile('rooms.json', JSON.stringify(rooms), 'utf8', (err) => {
-        if (err) {
-            console.log(err)
-        }
+    // Metodos para uso do socket
+    server.on("error", (err) => {
+        console.log(`server error:\n${err.stack}`);
+        server.close();
     });
-}
 
-// Le sala do arquivo Json
-const readRooms = () => {
-    rooms = JSON.parse(fs.readFileSync("rooms.json", "utf-8"));
-}
+    server.on("message", (msg, rinfo) => {
+        readRooms();
+        const message = Buffer.from(`${rinfo.address}:${rinfo.port}: ${msg.toString().split("|")[1]}`);
+        console.log(`${message}`);
+        const roomSelect = `${msg.toString().split("|")[0]}`;
 
-server.on("error", (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-});
+        if(!rooms[roomSelect]) {
+            rooms[roomSelect] = [rinfo.port];
+            writeRooms();
+        } else {
+            if (rooms[roomSelect].indexOf(rinfo.port) === -1) {
+                rooms[roomSelect].push(rinfo.port);
+                writeRooms();
+            }
+            for (const user of rooms[roomSelect]) {
+                if(user !== rinfo.port)
+                server.send(message, user, host);
+            }
+        }
 
-server.on("message", (msg, rinfo) => {
-    readRooms();
-    const message = Buffer.from(`${rinfo.address}:${rinfo.port}: ${msg.toString().split("|")[1]}`);
-    console.log(`${message}`);
-    const roomSelect = `${msg.toString().split("|")[0]}`;
-
-    if(!rooms[roomSelect]) {
-        rooms[roomSelect] = [rinfo.port];
-        writeRooms();
-    } else {
-        if (rooms[roomSelect].indexOf(rinfo.port) === -1) {
-            rooms[roomSelect].push(rinfo.port);
+        if(msg.toString().split("|")[1] === "--- Saiu da Sala ---") {
+            const index = rooms[roomSelect].indexOf(rinfo.port);
+            rooms[roomSelect].splice(index, 1);
             writeRooms();
         }
-        for (const user of rooms[roomSelect]) {
-            if(user !== rinfo.port)
-            server.send(message, user, host);
-        }
-    }
+    });
 
-    if(msg.toString().split("|")[1] === "--- Saiu da Sala ---") {
-        const index = rooms[roomSelect].indexOf(rinfo.port);
-        rooms[roomSelect].splice(index, 1);
-        writeRooms();
-    }
-});
-
-server.on("listening", () => {
-    const address = server.address();
-    console.log(`Server listening ${address.address}:${address.port}`);
-});
-server.bind({
-    address: host,
-    port: portServer
-});
-  
+    server.on("listening", () => {
+        const address = server.address();
+        console.log(`Server listening ${address.address}:${address.port} - Worker ${process.pid} started`);
+    });
+    server.bind({
+        address: host,
+        port: portServer
+    });
+}
